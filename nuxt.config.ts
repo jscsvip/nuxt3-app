8bd9d096b7e6776046a14e5a6820f9bef98c2f16
李龙 <lilong2576@hanweb.com>
2025-02-24 17:35:24 +0800

    feat: sass稳定版与sass变量的使用

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: [
-    'assets/global.css'
-  ]
+    'assets/global.scss'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "~/assets/_variables.scss";',
+        },
+      },
+    },
+  },
 })
