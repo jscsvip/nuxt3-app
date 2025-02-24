5d8c3d591852bee9f5ee75986a148e541c18a74b
李龙 <lilong2576@hanweb.com>
2025-02-24 18:06:18 +0800

    feat: 使用nuxt/ui进行开发

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-    <div>技术成神 首页page</div>
+    <div class="flex items-center flex-col gap-2">
+    <h1>技术成神-首页</h1>
+    <div>
+      <UButton>Button</UButton>
+    </div>
     <NuxtLink to="/detail">Detail Page</NuxtLink> <br/>
     <NuxtLink to="/detail-1">路由参数:detail 1</NuxtLink> <br/>
     <NuxtLink to="/blogs">博客首页</NuxtLink> <br/>
     <NuxtLink to="/blogs/1">博客详情:blogs-1</NuxtLink> <br/>
-
+  </div>
 </template>
