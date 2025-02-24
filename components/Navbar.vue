051f27f09418579cb882e9c62443fb88bf383ba6
李龙 <lilong2576@hanweb.com>
2025-02-24 17:54:23 +0800

    feat: 组件拆分与自动导入

---
+++ components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+   <nav
+      class="border-b border-slate-200 px-5 py-2 flex items-center justify-between"
+    >
+      <h1 class="text-2xl font-bold">技术成神-Nuxt3</h1>
+      <img
+        class="w-[50px] border-[1px] border-slate-300 rounded-full inline-block"
+        src="~/assets/afei.jpg"
+        alt="avatar"
+      />
+    </nav>
+</template>
