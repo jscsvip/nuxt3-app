805339a7f874ab0e04232e00abde9c4233458813
李龙 <lilong2576@hanweb.com>
2025-02-24 17:46:33 +0800

    style: 调整nav的样式

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <nav>
-      <img class="avatar  inline-block" src="~/assets/afei.jpg" alt="avatar" >
-      导航栏
-      <img class="w-[50px] border-[1px] border-slate-300 rounded-full inline-block" src="/favicon.ico" alt="avatar" >
+    <nav
+      class="border-b border-slate-200 px-5 py-2 flex items-center justify-between"
+    >
+      <h1 class="text-2xl font-bold">技术成神-Nuxt3</h1>
+      <img
+        class="w-[50px] border-[1px] border-slate-300 rounded-full inline-block"
+        src="~/assets/afei.jpg"
+        alt="avatar"
+      />
     </nav>
     <slot />
   </div>
