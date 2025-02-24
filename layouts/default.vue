1545255cbcd63bbbac876e34e7b4aed020646e4e
李龙 <lilong2576@hanweb.com>
2025-02-24 17:06:43 +0800

    feat: 加载静态资源文件的两种方法

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <nav>导航栏</nav>
+    <nav>
+      <img class="avatar" src="~/assets/afei.jpg" alt="avatar" >
+      导航栏
+      <img class="avatar" src="/favicon.ico" alt="avatar" >
+    </nav>
     <slot />
   </div>
 </template>
+<style scoped>
+.avatar {
+  width: 50px;
+  border: 1px solid rgb(218, 218, 218);
+  border-radius: 50%;
+}
+</style>
