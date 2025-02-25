95464d2cc619a247d6d94dcad52d361cd721f1f4
jscsvip <jscs_vip@163.com>
2025-02-25 22:17:14 +0800

    feat: 切换主题

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,14 +3,29 @@
     <!-- <Navbar></Navbar> -->
     
     <NavBar></NavBar>
-
+    <div>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="sepia">Sepia</option>
+    </select>
+  </div>
     <slot />
   </div>
 </template>
-<style scoped>
-.avatar {
-  width: 50px;
-  border: 1px solid rgb(218, 218, 218);
-  border-radius: 50%;
+<script setup>
+const colorMode = useColorMode();
+console.log(colorMode.preference);
+</script>
+<style>
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
 }
 </style>
