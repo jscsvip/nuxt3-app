942c55f6e077d59d34733bb49ab7dab3ba8c620b
李龙 <lilong2576@hanweb.com>
2025-02-25 09:34:19 +0800

    feat: 请求本地的file并做成接口

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="flex items-center flex-col gap-2">
     <h1>技术成神-首页</h1>
+    <div v-for="post in posts" :key="post.id">
+      <NuxtLink class="text-lg" :to="`/detail/${post.id}`">{{
+        post.title
+      }}</NuxtLink>
+      <p class="text-slate-500">发布于: {{ post.date }}</p>
+    </div>
     <div>
       <UButton>Button</UButton>
     </div>
@@ -8,9 +14,11 @@
     <NuxtLink to="/detail-1">路由参数:detail 1</NuxtLink> <br/>
     <NuxtLink to="/blogs">博客首页</NuxtLink> <br/>
     <NuxtLink to="/blogs/1">博客详情:blogs-1</NuxtLink> <br/>
+    
   </div>
   {{ message }}
 </template>
 <script setup lang="ts">
     const { message } = await $fetch('/api/hello')
+    const posts = await $fetch("/api/posts");
 </script>
