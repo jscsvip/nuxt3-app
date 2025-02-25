816cdc85065396d172aa12366de2e414c52223f3
jscsvip <jscs_vip@163.com>
2025-02-26 00:13:38 +0800

    feat: 使用prisma完成登录查询用户的功能

---
+++ pages/login2.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const email = useState(() => '')
+const onLogin = () => {
+  $fetch('/api/login', {
+    method: 'post',
+    body: {
+      email: email.value,
+    },
+  }).then((user) => {
+    console.log(user)
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      输入的邮箱{{ email }}
+    </div>
+    <input v-model="email" />
+    <UButton @click="onLogin">
+      登录
+    </UButton>
+  </div>
+</template>
